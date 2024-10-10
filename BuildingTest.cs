da0a867fb4906c62ddd3cb9d8a70b0b3f9222428
berns021 <bernardvicc@yahoo.com>
2024-10-11 04:54:49 +0800

    Add project files.

---
+++ BuildingTest.cs
@@ -0,0 +1,47 @@
+﻿using ElevatorManagementSystem.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ElevatorManagementSystem.Tests
+{
+    [TestClass]
+    public class BuildingTests
+    {
+        [TestMethod]
+        public void Building_Initialization_SetsCorrectProperties()
+        {
+            // Arrange
+            int floors = 10;
+            int elevatorCount = 2;
+
+            // Act
+            Building building = new Building(floors, elevatorCount);
+
+            // Assert
+            Assert.AreEqual(floors, building.Floors);
+            Assert.AreEqual(elevatorCount, building.Elevators.Count);
+            foreach (var elevator in building.Elevators)
+            {
+                Assert.AreEqual(floors, elevator.Floors);
+            }
+        }
+
+        [TestMethod]
+        public void SelectElevator_ReturnsClosestElevator()
+        {
+            // Arrange
+            int floors = 10;
+            Building building = new Building(floors, 3);
+
+            // Act
+            building.Elevators[0].CurrentFloor = 1; // First elevator is on the ground floor
+            building.Elevators[1].CurrentFloor = 5; // Second elevator is on the fifth floor
+            building.Elevators[2].CurrentFloor = 10; // Third elevator is on the top floor
+
+            Elevator selectedElevator = building.SelectElevator(3, true); // Requesting floor 3, going up
+
+            // Assert
+            Assert.AreEqual(1, selectedElevator.CurrentFloor); // Should select the first elevator
+        }
+
+    }
+}
---
+++ ElevatorTests.cs
@@ -0,0 +1,93 @@
+﻿using ElevatorManagementSystem.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace ElevatorSystemManagementTests
+{
+    [TestClass]
+    public class ElevatorTests
+    {
+        [TestMethod]
+        public void Elevator_InitialFloor_ShouldBeOne()
+        {
+            // Arrange
+            Elevator elevator = new Elevator(1, 10);
+
+            // Act
+            int initialFloor = elevator.CurrentFloor;
+
+            // Assert
+            Assert.AreEqual(1, initialFloor);
+        }
+
+        [TestMethod]
+        public void Elevator_AddRequest_ShouldUpdateTargetFloors()
+        {
+            // Arrange
+            Elevator elevator = new Elevator(1, 10);
+
+            // Act
+            elevator.AddRequest(5, true);
+
+            // Assert
+            Assert.AreEqual(1, elevator.TargetFloors.Count);
+            Assert.AreEqual(5, elevator.TargetFloors.First());
+        }
+
+        [TestMethod]
+        public void Elevator_DistanceTo_ShouldReturnCorrectDistance()
+        {
+            // Arrange
+            Elevator elevator = new Elevator(1, 10);
+            elevator.CurrentFloor = 3;
+
+            // Act
+            int distance = elevator.DistanceTo(7);
+
+            // Assert
+            Assert.AreEqual(4, distance);
+        }
+
+        [TestMethod]
+        public void Elevator_IsGoingInDirection_Up_ShouldReturnTrue()
+        {
+            // Arrange
+            Elevator elevator = new Elevator(1, 10);
+            elevator.CurrentFloor = 3;
+
+            // Act
+            bool isGoingUp = elevator.IsGoingInDirection(true, 7);
+
+            // Assert
+            Assert.IsTrue(isGoingUp);
+        }
+
+        [TestMethod]
+        public void Elevator_IsGoingInDirection_Down_ShouldReturnFalse()
+        {
+            // Arrange
+            Elevator elevator = new Elevator(1, 10);
+            elevator.CurrentFloor = 7;
+
+            // Act
+            bool isGoingUp = elevator.IsGoingInDirection(true, 3);
+
+            // Assert
+            Assert.IsFalse(isGoingUp);
+        }
+
+        [TestMethod]
+        public void Elevator_MoveToFloor_ShouldReachTargetFloor()
+        {
+            // Arrange
+            Elevator elevator = new Elevator(1, 10);
+
+            // Act
+            Task.Run(() => elevator.MoveToFloor(5)).Wait();
+
+            // Assert
+            Assert.AreEqual(5, elevator.CurrentFloor);
+        }
+    }
+}
